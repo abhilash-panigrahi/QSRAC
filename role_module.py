11ef877d44e4548c221b97ed4f56fb3a526ceac4
Abhilash Panigrahi <abhilash.panigrahi2022@vitstudent.ac.in>
2026-04-25 00:28:38 +0530

    ...

--- role_module.py
+++
@@ -1,8 +0,0 @@
-ALLOWED_ROLES = {"user", "admin"}
-
-
-def validate_role(core_token: dict) -> bool:
-    role = core_token.get("role")
-    if not role:
-        return False
-    return role in ALLOWED_ROLES
